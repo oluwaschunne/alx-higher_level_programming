b756f145c1784fc443b528175e378549980c43a1
oluwaschunne <falopeoluwaseun2@gmail.com>
2023-06-11 14:53:10 -0700

    data structures

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def delete_at(my_list=[], idx=0):
+    if not my_list:  # Check if the list is empty
+        return my_list
+
+    if idx < 0 or idx >= len(my_list):  # Check if idx is negative or out of range
+        return my_list
+
+    del my_list[idx]  # Delete the item at the specified index
+
+    return my_list
