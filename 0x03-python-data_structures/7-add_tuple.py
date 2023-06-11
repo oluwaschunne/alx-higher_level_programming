07458f9dfcee0b203494391f6cdd3b9e56c5e9b7
oluwaschunne <falopeoluwaseun2@gmail.com>
2023-06-11 10:26:24 -0700

    mul returns

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,11 +1,11 @@
-#!/use/bin/python3
+#!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
     # Extend the tuples to have a length of at least 2
     tuple_a = tuple_a[:2] + (0, 0)
     tuple_b = tuple_b[:2] + (0, 0)
-    
+
     # Perform the addition of corresponding elements
     result = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    
+
     return result
