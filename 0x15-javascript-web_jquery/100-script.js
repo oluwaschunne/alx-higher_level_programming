24eeaceeed2559158a6d0ac99d495336bef58582
oluwaschunne <falopeoluwaseun2@gmail.com>
2023-11-01 14:59:16 -0700

    jquery

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Select the <header> element using document.querySelector
+    let headerElement = document.querySelector("header");
+    
+    // Update the text color to red
+    headerElement.style.color = "#FF0000";
+  });
