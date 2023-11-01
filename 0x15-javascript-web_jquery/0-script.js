008f8d679b73234762d4ebc109b1bca0b94a5525
oluwaschunne <falopeoluwaseun2@gmail.com>
2023-11-01 14:13:14 -0700

    jquery

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,5 @@
+// Get the <header> element using document.querySelector
+const headerElement = document.querySelector("header");
+
+// Update the text color to red
+headerElement.style.color = "#FF0000";
